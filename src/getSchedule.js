5e7a38ebde743c65b37a28def8bd96c0a44b96cd
Fernanda Ferreira <fefcufe@gmail.com>
2022-01-26 14:34:16 -0300

    corrigindo lint 2

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,11 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
-  if (scheduleTarget === undefined) {
+  /* if (scheduleTarget === undefined) {
     const days = Object.keys(data.hours);
     const hourSchedule = {};
     days.forEach((day) => hourSchedule[day] = )
-  }
+  } */
 }
-console.log(getSchedule());
+/* console.log(getSchedule()); */
 module.exports = getSchedule;
