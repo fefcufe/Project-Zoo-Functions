f0695e271b105288f5b9bff0a435496cbfc898d4
Fernanda Ferreira <fefcufe@gmail.com>
2022-01-26 13:58:53 -0300

    requisito 9 entregue

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,18 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+function getOldestFromFirstSpecies(employeeIdentification) {
+  const employeeFromId = employees.find(({ id }) => (id === employeeIdentification));
+  const firstAnimal = employeeFromId.responsibleFor[0];
+  const firstAnimalinSpecies = species.find(({ id }) => id === firstAnimal);
+  const oldestResident = firstAnimalinSpecies.residents.reduce((acc, currentValue) => {
+    if (acc.age > currentValue.age) {
+      return acc;
+    } return currentValue;
+  });
+  const valuesOfOldestResident = Object.values(oldestResident);
+  return valuesOfOldestResident;
 }
+
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (scheduleTarget === undefined) {
+    const days = Object.keys(data.hours);
+    const hourSchedule = {};
+    days.forEach((day) => hourSchedule[day] = )
+  }
 }
-
+console.log(getSchedule());
 module.exports = getSchedule;
