f10b2248d87a4c52b0810b729b50be12295b381f
Fernanda Ferreira <fefcufe@gmail.com>
2022-01-26 08:32:12 -0300

    requisito 4 terminado AMÉM

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -6,14 +6,10 @@ function isManager(id) {
 
 function getRelatedEmployees(managerId) {
   if (isManager(managerId) === true) {
-    // acessar cada [managers] de cada objeto em employee
-    employees.managers.map((manager) => {
-      if (manager === managerId) {
-        return `${employees.firstName} ${employees.lastName}`;
-      }
-    });
-  }
+    const listOfSubordinates = employees.filter(({ managers }) => managers.includes(managerId));
+    return listOfSubordinates.map(({ firstName, lastName }) => `${firstName} ${lastName}`);
+  } throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
-console.log(getRelatedEmployees(stephanieId));
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 module.exports = { isManager, getRelatedEmployees };
