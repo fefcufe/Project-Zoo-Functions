172a39c8e5a1b2d1a56e89eeaca372b451008b65
Fernanda Ferreira <fefcufe@gmail.com>
2022-01-26 14:28:52 -0300

    corrigindo lint

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,10 +1,6 @@
-const { species } = require('../data/zoo_data');
+const data = require('../data/zoo_data');
 
 function countAnimals(animal = 'none') {
-  if (animal === 'none') {
-    const thisAnimal = {};
-    return species.map(({ name, popularity }) => (thisAnimal[name] = popularity));
-  }
 }
-console.log(countAnimals());
+
 module.exports = countAnimals;
