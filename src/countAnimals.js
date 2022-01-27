5ba78aa17117e0c7604bc2274c9741556a405b16
Fernanda Ferreira <fefcufe@gmail.com>
2022-01-26 21:39:27 -0300

    requisito 5 entregue

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,6 +1,19 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function countAnimals(animal = 'none') {
+function countAnimals(animal) {
+  if (animal === undefined) {
+    return species.reduce((acc, currentValue) => {
+      acc[currentValue.name] = currentValue.residents.length;
+      return acc;
+    }, {});
+  }
+  if (Object.keys(animal).length === 1) {
+    return species.find((specieObject) => specieObject.name === animal.specie).residents.length;
+  }
+  if (Object.keys(animal).length === 2) {
+    const animalChoice = species.find((specieObject) => specieObject.name === animal.specie);
+    return animalChoice.residents.filter((element) => element.sex === animal.sex).length;
+  }
 }
-
+console.log(countAnimals({ specie: 'lions', sex: 'male' }));
 module.exports = countAnimals;
